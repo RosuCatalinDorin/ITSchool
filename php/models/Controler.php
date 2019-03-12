dbcb36735f11c452f798b54b3e4b883bf2bbcd36
rosucatalindorin <rosucatalindorin@gmail.com>
2019-03-12 17:59:12 +0200

    change href to coures page

--- php/models/Controler.php
+++ php/models/Controler.php
@@ -2,19 +2,15 @@
   class Controler {
     // DB stuff
     private $conn;
-    private $table = 'posts';
+    private $tableUsers = 'users';
+    private $tableMK = 'marketing_users_details';
 
-      public $curs_id;
-      public $titlu_curs;
-      public $tehnologii;
-      public $data_start;
-      public $data_end;
-      public $msg_atentionare_date_start;
-      public $msg_pers_targetate;
-      public $pret;
-      public $pret_reducere;
-      public $numar_total_ore;
-      public $numar_ore_saptamana;
+      public $curs_id; public $titlu_curs; public $tehnologii; public $data_start;
+      public $data_end; public $msg_atentionare_date_start; public $msg_pers_targetate;
+      public $pret; public $pret_reducere; public $numar_total_ore; public $numar_ore_saptamana;
+      public $first_name; public $last_name; public $telefon; public $data_nasterii; public $email;
+      public $cunostinte_it; public $old_industry; public $nivel_engeleza; public $info_marketing_source;
+      public $accept_termeni_conditii;
     // Constructor with DB
     public function __construct($db) {
       $this->conn = $db;
@@ -69,32 +65,58 @@
     }
 
     // Create user
-    public function create() {
-          // Create query
-          $query = 'INSERT INTO ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id';
+    public function addCustomerToCourse() {
+
+         $query = "INSERT INTO ". $this->tableUsers."(first_name,last_name, phone,curs_id,email,birthday) values
+                                                     (:first_name,:last_name,:phone,:curs_id,:email,:birthday)";
           // Prepare statement
-          $stmt = $this->conn->prepare($query);
-          // Clean data
-          $this->title = htmlspecialchars(strip_tags($this->title));
-          $this->body = htmlspecialchars(strip_tags($this->body));
-          $this->author = htmlspecialchars(strip_tags($this->author));
-          $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+         $queryUserID = 'SELECT MAX(user_id) as ID_USER from users';
+         $queryInsertMarkDet="INSERT INTO ".$this->tableMK."(user_id,cunostinte_it,statut_acual, nivel_engeleza, info_marketing_source, accept_termeni_conditii)
+                                                     values (:user_id,:cunostinte_it,:statut_acual,:nivel_engeleza,:info_marketing_source,:accept_termeni_conditii)";
+         $connection =  $this->conn;
+         $connection->beginTransaction();
+         try {
+             $stmt = $connection->prepare($query);
+             // Clean data
+             $this->first_name = htmlspecialchars(strip_tags($this->first_name));
+             $this->last_name = htmlspecialchars(strip_tags($this->last_name));
+             $this->telefon = htmlspecialchars(strip_tags($this->telefon));
+             $this->curs_id = htmlspecialchars(strip_tags($this->curs_id));
+             $this->data_nasterii = htmlspecialchars(strip_tags($this->data_nasterii));
+             // Bind data
+             $stmt->bindParam(':first_name', $this->first_name);
+             $stmt->bindParam(':last_name', $this->last_name);
+             $stmt->bindParam(':phone', $this->telefon);
+             $stmt->bindParam(':curs_id', $this->curs_id);
+             $stmt->bindParam(':email', $this->email);
+             $stmt->bindParam(':birthday', $this->data_nasterii);
+             $stmt->execute();
 
-          // Bind data
-          $stmt->bindParam(':title', $this->title);
-          $stmt->bindParam(':body', $this->body);
-          $stmt->bindParam(':author', $this->author);
-          $stmt->bindParam(':category_id', $this->category_id);
+             $stmt= $connection->prepare($queryUserID);
+             $stmt->execute();
+             $ReSQLuserId = $stmt->fetch(PDO::FETCH_ASSOC);
 
-          // Execute query
-          if($stmt->execute()) {
-            return true;
-      }
+             $stmt = $connection->prepare($queryInsertMarkDet);
+             $this->cunostinte_it = htmlspecialchars(strip_tags($this->cunostinte_it));
+             $this->old_industry = htmlspecialchars(strip_tags($this->old_industry));
+             $this->nivel_engeleza = htmlspecialchars(strip_tags($this->nivel_engeleza));
+             $this->info_marketing_source = htmlspecialchars(strip_tags($this->info_marketing_source));
 
-      // Print error if something goes wrong
-      printf("Error: %s.\n", $stmt->error);
+             $stmt->bindParam(':user_id', $ReSQLuserId['ID_USER']);
+             $stmt->bindParam(':cunostinte_it', $this->cunostinte_it);
+             $stmt->bindParam(':statut_acual', $this->old_industry);
+             $stmt->bindParam(':nivel_engeleza', $this->nivel_engeleza);
+             $stmt->bindParam(':info_marketing_source', $this->info_marketing_source);
+             $stmt->bindParam(':accept_termeni_conditii', $this->accept_termeni_conditii);
+             $stmt->execute();
 
-      return false;
+             $connection->commit();
+             return true;
+         }
+         catch (PDOException $ex){
+             $connection->rollBack();
+             return $ex;
+         }
     }
 
     // Update Post
---
+++ php/view/addToCourse.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: catalin.rosu
+ * Date: 06/03/2019
+ * Time: 9:16 PM
+ */
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../config/Database.php';
+include_once '../models/Controler.php';
+
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate controlerobj
+$controler= new Controler($db);
+
+// Get raw posted data
+$data = json_decode(file_get_contents("php://input"));
+
+if($data == null){
+    $controler->curs_id = $_POST['curs_id'];
+    $controler->first_name = $_POST['first_name'];
+    $controler->last_name = $_POST['last_name'];
+    $controler->data_nasterii = $_POST['data_nasterii'];
+    $controler->cunostinte_it = $_POST['cunostinte_it'];
+    $controler->email = $_POST['email'];
+    $controler->telefon = $_POST['telefon'];
+    $controler->old_industry = $_POST['old_industry'];
+    $controler->nivel_engeleza = $_POST['nivel_engeleza'];
+    $controler->info_marketing_source = $_POST['info_marketing_source'];
+    $controler->accept_termeni_conditii = $_POST['accept_termeni_conditii'];
+}
+else {
+    $controler->curs_id = $data->curs_id;
+    $controler->first_name = $data->first_name;
+    $controler->last_name = $data->last_name;
+    $controler->data_nasterii = $data->data_nasterii;
+    $controler->cunostinte_it = $data->cunostinte_it;
+    $controler->email = $data->email;
+    $controler->telefon = $data->telefon;
+    $controler->old_industry = $data->old_industry;
+    $controler->nivel_engeleza = $data->nivel_engeleza;
+    $controler->info_marketing_source = $data->info_marketing_source;
+    $controler->accept_termeni_conditii = $data->accept_termeni_conditii;
+}
+
+
+// Create post
+if($controler->addCustomerToCourse() === true) {
+    echo json_encode(
+        array('STATUS' => 'SUCCES')
+    );
+} else {
+    echo json_encode(
+        array('STATUS' => $controler->addCustomerToCourse())
+    );
+}
+
