e390ec8e4e3543e914f77a76bfa07b8e3de462ad
rosucatalindorin <rosucatalindorin@gmail.com>
2019-03-27 18:22:54 +0200

    Edit WEB SERVICE

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 var tehFrontEnd = {
-    tehFolosite :["HTML/CSS/Java-Script","Programare orientata pe obiect","Git","Indrumare interviuri","jQuery","AJAX","HTTP - Requests"]
+    tehFolosite :["HTML/CSS/Java-Script","Programare orientata pe obiect","GitHub","jQuery","AJAX","HTTP - Requests","GitHub","Indrumare interviuri"]
 };
 
 var tehAdvanceJavascript = {
-    tehFolosite :["Java-Script","NodeJs","HTML","CSS","Design Patterns","Indrumare interviuri","Indrumare interviuri","Promises","HTTP - Requests"]
+    tehFolosite :["Java-Script","NodeJs","HTML","CSS","Design Patterns","Indrumare interviuri","Indrumare interviuri","Promises","HTTP - Requests","GitHub"]
 };
 
 var tehPhpSql = {
-    tehFolosite :["PHP","Apache","Server/Client","WebService","HTTP - Requests","Securitate","Browsers","Introducere în baze de date","Principii de design și arhitectură","Limbajul SQL","Stored Procedures","Database Administration","Database Optimization"]
+    tehFolosite :["PHP","Apache","Server/Client","WebService","HTTP - Requests","Securitate","Browsers","GitHub","Introducere în baze de date","Principii de design și arhitectură","Limbajul SQL","Stored Procedures","Database Administration","Database Optimization"]
 
 };
