ea157fe33111692898fd29b181c8b3e69fdd8298
rosucatalindorin <rosucatalindorin@gmail.com>
2019-04-03 10:44:08 +0300

    clone with host

--- detaliiCurs/js/detalii-curs.js
+++ detaliiCurs/js/detalii-curs.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  // let id = window.localStorage.getItem("curs_id");
-  let id = "4";
+  let id = window.localStorage.getItem("curs_id");
+  //let id = "4";
   detaliiCourseService.getCourse(id).then(function(course) {
     displayCourseDetailsInPage(course[0]);
   });
